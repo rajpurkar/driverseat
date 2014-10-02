2e27448eb786f3af55d95576510eb3c8e13dd40f
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-10-02 00:24:06 -0700

    adds car, motion, animation, camera target

--- public/demo.js
+++ public/demo.js
@@ -21,44 +21,44 @@ document.addEventListener('keyup', function(event) {
 /**
  * Algorithm to calculate a single RGB channel (0-255) from HSL hue (0-1.0)
  */
-function HUEtoRGB(hue) {
-	if (hue < 0) {
-		hue += 1;
-	} else if (hue > 1) {
-		hue -= 1;
-	}
-	var rgb = 0;
-	if (hue < 1/6) {
-		rgb = hue*6;
-	} else if (hue < 1/2) {
-		rgb = 1;
-	} else if (hue < 2/3) {
-		rgb = (2/3 - hue)*6;
-	}
-	return Math.round(rgb * 255);
-}
+ function HUEtoRGB(hue) {
+ 	if (hue < 0) {
+ 		hue += 1;
+ 	} else if (hue > 1) {
+ 		hue -= 1;
+ 	}
+ 	var rgb = 0;
+ 	if (hue < 1/6) {
+ 		rgb = hue*6;
+ 	} else if (hue < 1/2) {
+ 		rgb = 1;
+ 	} else if (hue < 2/3) {
+ 		rgb = (2/3 - hue)*6;
+ 	}
+ 	return Math.round(rgb * 255);
+ }
 
-function generatePointCloud() {
-	geometry = new THREE.BufferGeometry();
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", dataFile, false);
-	xhr.send(null);
-	if (xhr.status !== 200 && xhr.status !== 0) {
-		throw new Error(dataFile + " not found");
-	}
-	var data = JSON.parse(xhr.responseText);
-	var positions = new Float32Array(3*data.length);
-	var colors    = new Float32Array(3*data.length);
-	for (var i = 0; i < data.length; i++) {
+ function generatePointCloud() {
+ 	geometry = new THREE.BufferGeometry();
+ 	var xhr = new XMLHttpRequest();
+ 	xhr.open("GET", dataFile, false);
+ 	xhr.send(null);
+ 	if (xhr.status !== 200 && xhr.status !== 0) {
+ 		throw new Error(dataFile + " not found");
+ 	}
+ 	var data = JSON.parse(xhr.responseText);
+ 	var positions = new Float32Array(3*data.length);
+ 	var colors    = new Float32Array(3*data.length);
+ 	for (var i = 0; i < data.length; i++) {
 		//Note: order is changed
 		positions[3*i]   = data[i][2];	//x
 		positions[3*i+1] = data[i][0];	//y
 		positions[3*i+2] = data[i][1];	//z
 		// map intensity (0-120) to RGB
 		var hue = 1 - data[i][3]/120;	//TODO: fix intensity scaling
-		colors[3*i]   = HUEtoRGB(hue+1/3);	//r
-		colors[3*i+1] = HUEtoRGB(hue);		//g
-		colors[3*i+2] = HUEtoRGB(hue-1/3);	//b
+		colors[3*i+1]   = HUEtoRGB(hue+1/3);	//r
+		colors[3*i+2] = HUEtoRGB(hue);		//g
+		colors[3*i+0] = HUEtoRGB(hue-1/3);	//b
 	}
 
 	geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
@@ -70,10 +70,42 @@ function generatePointCloud() {
 	return pointcloud;
 }
 
+function addCar(){
+	
+	var loader = new THREE.PLYLoader();
+	loader.addEventListener( 'load', function ( event ) {
+
+		var geometry = event.content;
+		var material = new THREE.MeshBasicMaterial( {} );
+		var mesh = new THREE.Mesh( geometry, material );
+
+		mesh.position.set( -2, 30, -2 );
+		mesh.rotation.set( 0, Math.PI / 2, Math.PI );
+		mesh.scale.set( 1, 1, 1 );
+
+		//mesh.castShadow = true;
+		//mesh.receiveShadow = true;
+
+		scene.add( mesh );
+
+	} );
+	loader.load('/files/gtr.ply' );
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+
 function init() {
 	scene = new THREE.Scene();
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-	
+	cameraTarget = new THREE.Vector3(0, 200, 0 );
 	projector = new THREE.Projector();
 	raycaster = new THREE.Raycaster();
 
@@ -92,10 +124,13 @@ function init() {
 	// controls
 	camera.position.set(0,10,0);
 	controls = new THREE.OrbitControls(camera);
-	controls.target.set( 0, 100, 0 );
-	camera.lookAt(new THREE.Vector3(0,100,0));
-	
+	//controls.target.set( 0, 100, 0 );
+	//camera.lookAt(new THREE.Vector3(0,100,0));
+	addCar();
+
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+	window.addEventListener( 'resize', onWindowResize, false );
+
 }
 
 function onDocumentMouseMove( event ) {
@@ -105,8 +140,14 @@ function onDocumentMouseMove( event ) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
 
+function animate() {
+
+	requestAnimationFrame( animate );
+
+	render();
+}
+
 function render() {			
-	requestAnimationFrame(render);
 	
 	camera.updateMatrixWorld(true);
 	
@@ -130,9 +171,18 @@ function render() {
 			geometry.attributes.color.needsUpdate = true;
 		}
 	}
+	var timer = Date.now() * 0.0005;
+
+	//camera.position.y +=  Math.abs(Math.cos( timer )) *.3 ;
+	camera.position.y +=  0.3;
+	//camera.position.z += Math.cos( timer ) * 0.03;
+
+	camera.lookAt( cameraTarget );
+
+	renderer.render( scene, camera );
 
 	renderer.render(scene, camera);
 }
 
 init();
-render();
+animate();

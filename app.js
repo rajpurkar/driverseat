f74f065e697065693301ab770826188dca923cf5
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-10-06 13:10:44 -0700

    changes default path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'), {maxAge: '1d'}));
 
 app.get('/', function(req, res, next){
-   res.redirect('/potree/examples/example.html');
+   res.redirect('/demo.html');
 });
 
 // catch 404 and forward to error handler

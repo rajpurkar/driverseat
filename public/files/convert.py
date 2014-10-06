5080bc6af965c958cbe822d3c6034d13fa5060af
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2014-10-06 14:26:46 -0700

    adds gps conversion

--- public/files/convert.py
+++ public/files/convert.py
@@ -2,10 +2,15 @@ import numpy as np
 import json
 import sys
 
-def run(filepath):
+def runData(filepath):
 	data = np.load(filepath)['data']
 	data_subset = data[:1000000, :4]
 	json.dump(data_subset.tolist(), open('datafile.json', 'w'))
 
+def runGps(filepath):
+	data = np.load(filepath)['data'];
+	data_subset = data[:data.shape[0]/5, :3]
+	json.dump(data_subset.tolist(), open('gpsfile.json', 'w'))
+
 if __name__ == '__main__':
-	run(sys.argv[1])
+	runGps(sys.argv[1])

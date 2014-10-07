6d5fd0beef32669b67358800fb2762531e3d00e5
toki.migimatsu <toki.migimatsu@gmail.com>
2014-10-07 11:04:20 -0700

    recommit demo.js

--- public/demo.js
+++ public/demo.js
@@ -3,24 +3,28 @@
 var renderer;
 var camera;
 var scene;
-var geometry; 
+var geometries = {};	//TODO: make non-global
+var kdtrees = {};		//TODO: make non-global
+var laneClouds = [];	//TODO: make non-global
+var groundNormals = [];	//TODO: make non-global
 var pickLocation;
 var mouse = { x: 1, y: 1 };
 var projector, raycaster;
 var dataFile = "files/datafile.json";
 var gpsFile = "files/gpsfile.json";
 var lanesFile = "files/lanesfile.json";
+var planesFile = "files/planesfile.json";
 var shiftKey = false;
 var gpsData;
-var paused = false;
+var paused = true;
+var DRAG_RANGE = 3;
 
-// Detect when shift key is being pressed for painting
+//TODO: make keycode handler
 document.addEventListener('keydown', function(event) {
 	console.log(event.keyCode);
 	if (event.keyCode == 16){
 		shiftKey = true;
-	}
-	if (event.keyCode == 80){
+	} else if (event.keyCode == 80){
 		togglePause();
 	}
 }, false);
@@ -29,74 +33,130 @@ document.addEventListener('keyup', function(event) {
 		shiftKey = false;
 }, false);
 
+//TODO: make mouse event handler, decompose function, clean up globals
+document.addEventListener('mousedown', function(event) {
+	for (var i = 0; i < laneClouds.length; i++) {
+		var intersects = raycaster.intersectObject(laneClouds[i]);
+		if (intersects.length > 0) {
+			selectedPoint = intersects[0];
+			var pointColors = geometries["lane"+i].attributes.color.array;
+			var index = intersects[0].index;
+			pointColors[3*index] = 255;
+			pointColors[3*index+1] = 0;
+			pointColors[3*index+2] = 0;
+			geometries["lane"+i].attributes.color.needsUpdate = true;
+			var pointPos = selectedPoint.object.geometry.attributes.position.array;
+
+			var nearestPoints = kdtrees["lane"+i].nearest(pointPos.subarray(3*index, 3*index+3), 100, DRAG_RANGE);
+			selectedPositions = {};
+			selectedIndex = index;
+			for (var j = 0; j < nearestPoints.length; j++) {
+				index = nearestPoints[j][0].pos;
+				selectedPositions[index] = new Float32Array(pointPos.subarray(3*index, 3*index+3));
+			}
+			for (j = 0; j < planes.length; j++) {
+				intersects = raycaster.intersectObject(planes[j]);
+				if (intersects.length > 0) {
+					selectedPlane = intersects[0];
+					document.addEventListener('mousemove', dragPoint);
+					return;
+				}
+			}
+		}
+	}
+});
+function dragPoint(event) {
+	var intersects = raycaster.intersectObject(selectedPlane.object);
+	if (intersects.length > 0) {
+		// var index = selectedPoint.index;
+		var pointPosition = selectedPoint.object.geometry.attributes.position;
+		var newPos = new THREE.Vector3();
+		newPos.subVectors(intersects[0].point, selectedPlane.point);
+		for (var index in selectedPositions) {
+			var dist = distanceFunction(selectedPositions[selectedIndex], selectedPositions[index]);
+			var weight = (Math.cos(Math.PI/DRAG_RANGE * Math.sqrt(dist)) + 1)/2;
+			pointPosition.array[3*index] = weight * newPos.x + selectedPositions[index][0];
+			pointPosition.array[3*index+1] = weight * newPos.y + selectedPositions[index][1];
+			pointPosition.array[3*index+2] = weight * newPos.z + selectedPositions[index][2];
+		}
+		pointPosition.needsUpdate = true;
+	}
+}
+document.addEventListener('mouseup', function(event) {
+	document.removeEventListener('mousemove', dragPoint);
+	var pointColor = selectedPoint.object.geometry.attributes.color;
+	var index = selectedIndex;
+	pointColor.array[3*index] = 255;
+	pointColor.array[3*index+1] = 255;
+	pointColor.array[3*index+2] = 255;
+	pointColor.needsUpdate = true;
+});
 
 function togglePause(){
-	if(paused === false){
-		paused = true;	
-	}else{
-		paused = false;
-	}
-	
+	paused = !paused;
+}
+
+function distanceFunction(a, b) {
+	return (a[0]-b[0])*(a[0]-b[0]) + (a[1]-b[1])*(a[1]-b[1]) + (a[2]-b[2])*(a[2]-b[2]);
 }
 /**
  * Algorithm to calculate a single RGB channel (0-255) from HSL hue (0-1.0)
  */
- function HUEtoRGB(hue) {
- 	if (hue < 0) {
- 		hue += 1;
- 	} else if (hue > 1) {
- 		hue -= 1;
- 	}
- 	var rgb = 0;
- 	if (hue < 1/6) {
- 		rgb = hue*6;
- 	} else if (hue < 1/2) {
- 		rgb = 1;
- 	} else if (hue < 2/3) {
- 		rgb = (2/3 - hue)*6;
- 	}
- 	return Math.round(rgb * 255);
+function HUEtoRGB(hue) {
+	if (hue < 0) {
+		hue += 1;
+	} else if (hue > 1) {
+		hue -= 1;
+	}
+	var rgb = 0;
+	if (hue < 1/6) {
+		rgb = hue*6;
+	} else if (hue < 1/2) {
+		rgb = 1;
+	} else if (hue < 2/3) {
+		rgb = (2/3 - hue)*6;
+	}
+	return Math.round(rgb * 255);
  }
 
- function loadPoints(df){
+function loadPoints(df){
 	var xhr = new XMLHttpRequest();
- 	xhr.open("GET", df, false);
- 	xhr.send(null);
- 	if (xhr.status !== 200 && xhr.status !== 0) {
- 		throw new Error(df + " not found");
- 	}
- 	var data = JSON.parse(xhr.responseText);
- 	return data;
- }
+	xhr.open("GET", df, false);
+	xhr.send(null);
+	if (xhr.status !== 200 && xhr.status !== 0) {
+		throw new Error(df + " not found");
+	}
+	var data = JSON.parse(xhr.responseText);
+	return data;
+}
 
- function generatePointCloud(data, size, color) {
- 	var geometry = new THREE.BufferGeometry();
- 	var positions = new Float32Array(3*data.length);
- 	var colors    = new Float32Array(3*data.length);
- 	for (var i = 0; i < data.length; i++) {
+function generatePointCloud(name, data, size, color) {
+	geometries[name] = new THREE.BufferGeometry();
+	var positions = new Float32Array(3*data.length);
+	var colors    = new Float32Array(3*data.length);
+	for (var i = 0; i < data.length; i++) {
 		//Note: order is changed
 		positions[3*i]   = data[i][2];	//x
 		positions[3*i+1] = data[i][0];	//y
 		positions[3*i+2] = data[i][1];	//z
 		// map intensity (0-120) to RGB
-		if(data[i].length >= 4){
+		if (data[i].length >= 4) {
 			var hue = 1 - data[i][3]/120;	//TODO: fix intensity scaling
 			colors[3*i+1]   = HUEtoRGB(hue+1/3);	//r
 			colors[3*i+2] = HUEtoRGB(hue);		//g
 			colors[3*i+0] = HUEtoRGB(hue-1/3);	//b
-		}else{
-			colors[3*i+1] = color;	//r
-			colors[3*i+2] = color;		//g
+		} else {
+			colors[3*i+1] = color;
+			colors[3*i+2] = color;
 			colors[3*i+0] = color;
 		}
 	}
 
-	geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
-	geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
-	
+	geometries[name].addAttribute('position', new THREE.BufferAttribute(positions, 3));
+	geometries[name].addAttribute('color', new THREE.BufferAttribute(colors, 3));
 	var material = new THREE.PointCloudMaterial({ size: size, vertexColors: true });
-	var pointcloud = new THREE.PointCloud(geometry, material);
-	
+	var pointcloud = new THREE.PointCloud(geometries[name], material);
+
 	return pointcloud;
 }
 
@@ -133,6 +193,45 @@ function addCar(){
 	loader.load('/files/gtr.ply' );
 }
 
+function addPlanes() {
+	planesData = loadPoints(planesFile);
+	planes = [];
+	var groundGeometry = new THREE.Geometry();
+	for (var i = 0; i < planesData.length; i++) {
+		var normal = planesData[i];
+		var dy = normal[0],
+			dz = normal[1],
+			dx = normal[2],
+			y = normal[3],
+			z = normal[4],
+			x = normal[5];
+		var origin = new THREE.Vector3(x,y,z),
+			direction = new THREE.Vector3(dx,dy,dz),
+			end = new THREE.Vector3(x+dx,y+dy,z+dz);
+		direction.normalize();
+		var plane = new THREE.Plane();
+		plane.setFromNormalAndCoplanarPoint(direction, origin);
+		var planeGeometry = new THREE.PlaneGeometry(50, 50),
+			planeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, transparent: true });
+		plane = new THREE.Mesh(planeGeometry, planeMaterial);
+		// groundGeometry.vertices.push()
+		plane.position.set(x, y, z);
+		plane.lookAt(end);
+		plane.visible = false;
+		scene.add(plane);
+		planes.push(plane);
+
+		// var ray = new THREE.Ray(origin, direction);
+		// groundNormals.push(ray);
+		var material = new THREE.LineBasicMaterial({ color: 0x00ff00 }),
+			geometry = new THREE.Geometry();
+		geometry.vertices.push(origin);
+		geometry.vertices.push(end);
+		var line = new THREE.Line(geometry, material);
+		scene.add(line);
+	}
+}
+
 function onWindowResize() {
 
 	camera.aspect = window.innerWidth / window.innerHeight;
@@ -150,22 +249,26 @@ function init() {
 	cameraTarget = target;
 	projector = new THREE.Projector();
 	raycaster = new THREE.Raycaster();
-	renderer = new THREE.WebGLRenderer({alpha:true});
-	renderer.setClearColor(0, 1)
+	renderer = new THREE.WebGLRenderer();
+	// renderer = new THREE.WebGLRenderer({alpha:true});
+	// renderer.setClearColor(0, 1);
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 	
-	var pointData = loadPoints(dataFile)
-	pointcloud = generatePointCloud(pointData, 0.01);
+	var pointData = loadPoints(dataFile);
+	pointcloud = generatePointCloud("points", pointData, 0.01);
 	scene.add(pointcloud);
-	gpsData = loadPoints(gpsFile)
-	gpsCloud = generatePointCloud(gpsData, 0.05, 250);
+	gpsData = loadPoints(gpsFile);
+	gpsCloud = generatePointCloud("gps", gpsData, 0.05, 250);
 	lanesData = loadPoints(lanesFile);
 	for (var lane in lanesData){
-		laneCloud = generatePointCloud(lanesData[lane], 0.15, 255);
+		var laneCloud = generatePointCloud("lane"+lane, lanesData[lane], 0.15, 255);
 		scene.add(laneCloud);	
+		laneClouds.push(laneCloud);
+		var positions = laneCloud.geometry.attributes.position.array;
+		kdtrees["lane"+lane] = new THREE.TypedArrayUtils.Kdtree(positions, distanceFunction, 3);
 	}
-	
+	addPlanes();
 
 	var sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
 	var sphereMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, shading: THREE.FlatShading});
@@ -179,11 +282,9 @@ function init() {
 	//controls = new THREE.OrbitControls(camera);
 	//controls.target.set( 0, 100, 0 );
 	//camera.lookAt(new THREE.Vector3(0,100,0));
-	
 
 	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 	window.addEventListener( 'resize', onWindowResize, false );
-
 }
 
 function onDocumentMouseMove( event ) {
@@ -194,15 +295,12 @@ function onDocumentMouseMove( event ) {
 }
 
 function animate() {
-
 	requestAnimationFrame( animate );
-
 	render();
 }
 var count = 0;
 
-function render() {			
-	
+function render() {
 	camera.updateMatrixWorld(true);
 	
 	vector = new THREE.Vector3( mouse.x, mouse.y, 0.5 );
@@ -212,17 +310,17 @@ function render() {
 	
 	var intersects = raycaster.intersectObject(pointcloud);
 	if(intersects.length > 0){
-		pickLocation.position.copy(intersects[0].point);
+		// pickLocation.position.copy(intersects[0].point); // brush cursor
 		// Paint the points with the cursor while pressing <shift>
 		if (shiftKey) {
-			var pointColors = geometry.attributes.color.array;
+			var pointColors = geometries.points.attributes.color.array;
 			for (var i = 0; i < intersects.length; i++) {
 				var index = 3*intersects[i].index;
 				pointColors[index] = 255;
 				pointColors[index+1] = 255;
 				pointColors[index+2] = 255;
 			}
-			geometry.attributes.color.needsUpdate = true;
+			geometries.points.attributes.color.needsUpdate = true;
 		}
 	}
 	//var timer = Date.now() * 0.0005;
@@ -238,10 +336,10 @@ function render() {
 	//camera.position.set(new THREE.Vector3(positionArr[2],positionArr[0],positionArr[1]));
 	//camera.lookAt(new THREE.Vector3(0,100,0));
 	camera.lookAt(target);
-	renderer.render( scene, camera );
+	// renderer.render( scene, camera );
 
 	renderer.render(scene, camera);
-	if(paused == false){
+	if(!paused){
 		count++;	
 	}
 	if(count+5 >= gpsData.length){

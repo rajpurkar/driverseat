9f20372fd1908ac202e028a23c7e14c7c4bf1526
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2015-04-06 16:18:10 -0700

    videos lint update cleaning unused vars

--- public/video.js
+++ public/video.js
@@ -1,37 +1,36 @@
-myApp.
-service('video', function() {
-    var ctx, canvas, video;
-    var images = new Array();
-    var zLoader;
-    var currentImage = new Image();
-    var maxFrameNum = 0;
-    var scaling = 1;
+(function (window, Image, JSZip) {
+  window.myApp.
+  service('video', function () {
+    var images = [],
+    scaling = 1
+
+    var init = function (data) {
+      var zLoader = new JSZip(data),
+      files = zLoader.file(/jpg/)
+
+      files.forEach(function (item) { // console.log(item)
+        var frameNum = parseInt(item.name.split('.jpg')[0], 10)
+        images[frameNum] = item.asBinary()
+      })
+    }
+
+    var displayImage = function (canvasId, frameNum) {
+      var j = new Image()
+      j.onload = function () {
+        var c = document.getElementById(canvasId)
+        var ctx = c.getContext('2d')
+        c.width = j.width * scaling
+        c.height = j.height * scaling
+        ctx.drawImage(j, 0, 0, c.width, c.height)
+      }
+
+      j.src = 'data:image/jpg;base64,' + window.btoa(images[frameNum])
+      return true
+    }
 
     return {
-        init: function(data) {
-            zLoader = new JSZip(data);
-            video = document.getElementById("video");
-            canvas = document.getElementById("projectionCanvas");
-            ctx = canvas.getContext("2d");
-            files = zLoader.file(/jpg/);
-            files.forEach(function(item) {
-                //console.log(item);
-                var frameNum = parseInt(item.name.split('.jpg')[0]);
-                images[frameNum] = item.asBinary();
-            });
-
-        },
-        displayImage: function(canvasId, frameNum) {
-            var j = new Image();
-            j.onload = function() {
-                var c = document.getElementById(canvasId);
-                var ctx = c.getContext("2d");
-                c.width = j.width * scaling;
-                c.height = j.height * scaling;
-                ctx.drawImage(j, 0, 0, c.width, c.height);
-            };
-            j.src = "data:image/jpg;base64," + btoa(images[frameNum]);
-            return true;
-        },
-    };
-});
+      init: init,
+      displayImage: displayImage
+    }
+  })
+})(window, window.Image, window.JSZip)

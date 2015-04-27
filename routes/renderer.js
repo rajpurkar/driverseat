bc48862ad7cca9b3ee7d6068bc3abeb94d42ea00
toki.migimatsu <toki.migimatsu@gmail.com>
2015-04-27 11:55:58 -0700

    choose video file to load based on which files exist

--- routes/renderer.js
+++ routes/renderer.js
@@ -2,6 +2,7 @@ var Category = require('./category')
 var util = require('./util')
 var db = require('./db')
 var Tag = require('./tag')
+var fs = require('fs')
 
 function edits (req, res, guest) {
   Category.find({}, function (err, categories) {
@@ -18,12 +19,27 @@ function edits (req, res, guest) {
 
     // var lanesFile = db.getLatestEdit(track)
     var datafilesPath = '/runs/' + track + '/'
+
+    // find video file (cam_2.mpg, cam_604.mpg, *.mpg...)
+    var videoPath = null
+    if (fs.existsSync('./public' + datafilesPath + 'cam_2.mpg')) {
+        videoPath = 'cam_2.mpg'
+    } else if (fs.existsSync('./public' + datafilesPath + 'cam_604.mpg')) {
+        videoPath = 'cam_604.mpg'
+    } else {
+        var files = fs.readdirSync('./public' + datafilesPath)
+        files = files.filter(function(elem) {
+            return elem.match("\.mpg$")
+        })
+        if (files.length > 0)
+            videoPath = files[0]
+    }
     var dataFiles = {
       points: datafilesPath + 'map.json.zip',
       gps: datafilesPath + 'gps.json.zip',
       lanes: datafilesPath + 'lanes/' + lanesFile,
       planes: datafilesPath + 'planes.json.zip',
-      video: datafilesPath + 'cam_2.mpg',
+      video: videoPath ? datafilesPath + videoPath : null,
       radar: datafilesPath + 'radar.json.zip',
       carDetection: datafilesPath + 'bbs-cam2.json',
       carDetectionVerified: datafilesPath + 'bbs-cam2-verified.json',

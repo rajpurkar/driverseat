0d9cd1e3a0ffa5cbccb985e42f44013bdac2f254
Pranav Rajpurkar <pranav.rajpurkar@gmail.com>
2015-05-08 02:12:52 -0700

    adds module for aggregating tags

---
+++ postprocess/tagAggegate.js
@@ -0,0 +1,32 @@
+var db = require('../routes/db')
+var Category = require('../routes/category')
+var fs = require('fs')
+var moment = require('moment')
+
+var main = function (callback) {
+  db.initdb() // setup connection to the database
+  Category.find({}, function (err, categories) {
+    if (err) {
+      console.error(err)
+    } else {
+      var folder_name = moment().format('ll')
+      fs.mkdirSync('./' + folder_name)
+      categories.forEach(function (cat) {
+        var wstream = fs.createWriteStream('./' + folder_name + '/' + cat.name + '.txt', { flags: 'w'})
+        cat.tags.forEach(function (tag) {
+          var runAndTrack = tag.run + '/' + tag.track
+          var startAndEndFrame = tag.startFrame + ' ' + tag.endFrame
+          wstream.write(runAndTrack + ' ' + startAndEndFrame + '\n')
+        })
+        wstream.end()
+      })
+      callback && callback()
+    }
+  })
+}
+
+if (require.main === module) {
+  main(function () {
+    db.closedb()
+  })
+}
